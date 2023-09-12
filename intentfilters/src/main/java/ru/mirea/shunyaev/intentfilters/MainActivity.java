7465b3e290ab33800b8c8ef85edf596c517037c6
Denis <122615902+ssteryally@users.noreply.github.com>
2023-09-12 15:50:17 +0300

    Add files via upload

---
+++ intentfilters/src/main/java/ru/mirea/shunyaev/intentfilters/MainActivity.java
@@ -0,0 +1,32 @@
+package ru.mirea.shunyaev.intentfilters;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onClickNewActivity(View view) {
+        Uri address = Uri.parse("https://www.mirea.ru/");
+        Intent openLinkIntent = new Intent(Intent.ACTION_VIEW, address);
+        startActivity(openLinkIntent);
+    }
+
+    public void onClickNewActivity2(View view) {
+        Intent shareIntent = new Intent(Intent.ACTION_SEND);
+        shareIntent.setType("text/plain");
+        shareIntent.putExtra(Intent.EXTRA_SUBJECT, "MIREA");
+        shareIntent.putExtra(Intent.EXTRA_TEXT, "Shunyaev Denis Dmitrievich");
+        startActivity(Intent.createChooser(shareIntent, "ФИО"));
+    }
+}
